c2153ab811502cdd0bca0af9ae8dad91cefa8de9
Armando Maldonado <maga_2814@hotmail.com>
2022-10-26 23:55:43 -0500

    EntregaServidorExpress

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const Contenedor = require("./Contenedor.js")
 
+//
+const express = require ('express');
+const aplicacion = express();
+const port = 8080;
+
 let contenedor= new Contenedor("productos.txt")
 
 let producto1 ={
@@ -25,9 +30,29 @@ metodos=async()=>{
     console.log( await contenedor.save(producto1))
     console.log( await contenedor.save(producto2))
     console.log( await contenedor.save(producto3))
-    console.log( await contenedor.getAll())
+    console.log( await contenedor.getAll(3))
     await contenedor.getById(2)
     console.log( await contenedor.deleteById(3))
-    console.log( await contenedor.deleteAll())
+    //console.log( await contenedor.deleteAll(1))
 }  
- metodos()
+ metodos()  
+ //let contenedor= new Contenedor("productos.txt")
+
+ aplicacion.get('/productos', async (peticion, respuesta) => {
+    const all= await contenedor.getAll();
+    respuesta.json(all);
+ });
+
+ aplicacion.get('/productosRandom', async (peticion, respuesta) => {
+  const all = await contenedor.getAll();
+  const random = Math.floor(Math.random() * all.length);
+  respuesta.json({
+    random: random
+  });
+});
+
+ const servidor = aplicacion.listen(port, () => {
+    console.log(`Servidor escuchando: ${servidor.address().port}`);
+ });
+
+ servidor.on('error', error => console.log(`Error: ${error}`));
